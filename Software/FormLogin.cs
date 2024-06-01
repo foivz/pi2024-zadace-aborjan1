962f82dfdb0638281b59f7436b13ca5adb2db7d4
aborjan1 <aborjan22@student.foi.hr>
2024-06-01 21:31:12 +0200

    Implementiaro sam login za radnika

---
+++ Software/FormLogin.cs
@@ -0,0 +1,76 @@
+﻿using RecycloSmart.Models;
+using DBLayer;
+using RecycloSmart.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace RecycloSmart
+{
+    public partial class FormLogin : Form
+    {
+
+        public static Radnik Ulogiran {  get; set; }
+
+
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno!", "Problem",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Lozinka nije unesena!", "Problem", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+            else
+            {
+              
+                Ulogiran = RadnikRepozitori.GetRadnik(txtPassword.Text);
+
+                if (Ulogiran != null)
+                {
+                    MessageBox.Show($"Retrieved: {Ulogiran.Lozinka}, Expected: {txtPassword.Text}", "Debug", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("User not found!", "Debug", MessageBoxButtons.OK);
+                }
+
+                if (Ulogiran != null && Ulogiran.Lozinka.Trim().Equals(txtPassword.Text.Trim(), StringComparison.OrdinalIgnoreCase))
+                {
+                    FormRadnik formaRadnika = new FormRadnik();
+                    Hide();
+                    formaRadnika.ShowDialog();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Krivi podaci!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Software/FormRadnik.cs
+++ Software/FormRadnik.cs
@@ -16,5 +16,10 @@ namespace RecycloSmart
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
